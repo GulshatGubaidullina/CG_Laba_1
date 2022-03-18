423003879b439a32af3fb46786e554854d1e8a4e
Gulshat <gubgg@yandex.ru>
2022-03-18 17:59:15 +0500

    Добавьте файлы проекта.

---
+++ CG_Laba_1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <GL/freeglut.h>
+
+void RenderSceneCB() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+
+};
+
+int main(int argc,char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    
+    glutInitWindowSize(1024, 500);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Tutorial 01");
+    
+    glutDisplayFunc(RenderSceneCB);
+    glClearColor(0.8f, 0.2f, 0.6f, 0.0f);
+
+    glutMainLoop();
+
+
+
+    std::cout << "Hello World!\n";
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
