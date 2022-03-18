053e17901b25dc1a25c86f5cd6c10ed25fe5c81d
Gulshat <gubgg@yandex.ru>
2022-03-18 18:59:59 +0500

    2

--- CG_Laba_1.cpp
+++ CG_Laba_1.cpp
@@ -1,39 +1,66 @@
 ﻿#include <iostream>
+#include <stdio.h>
+#include <GL/glew.h>
 #include <GL/freeglut.h>
+#include "math_3d.h"
+#include "glm/glm.hpp"
+
+
+
+GLuint VBO;
 
 void RenderSceneCB() {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glClear(GL_COLOR_BUFFER_BIT);
-    glutSwapBuffers();
 
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glDrawArrays(GL_POINTS, 0, 1);
+
+    glDisableVertexAttribArray(0);
+
+    glutSwapBuffers();
 };
+
+static void CreateVertexBuffer()
+{
+    glm::vec3 Vertices[1];
+    Vertices[0] = glm::vec3(0.0f, 0.0f, 0.0f);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+}
+
 
 int main(int argc,char **argv)
 {
+    
+
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
     
+  
+
     glutInitWindowSize(1024, 500);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("Tutorial 01");
     
     glutDisplayFunc(RenderSceneCB);
-    glClearColor(0.8f, 0.2f, 0.6f, 0.0f);
+    
 
+    GLenum res = glewInit();
+    if (res != GLEW_OK)
+    {
+        fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+        return 1;
+    }
+    glClearColor(0.2f, 0.2f, 0.6f, 0.0f);
+    CreateVertexBuffer();
     glutMainLoop();
 
 
-
-    std::cout << "Hello World!\n";
+    return 0;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
